49612e07b5fee0392f52b87478a794f40827cf0e
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-14 10:55:13 -0300

    Add exercicio condicional 07

---
+++ Typescript/exercicioCondicional07.ts
@@ -0,0 +1,15 @@
+let media: number = 55;
+let frequencia: number = 90;
+let projetoFinal: number = 98;
+
+if(media >= 60 && frequencia >= 75 && projetoFinal > 0){
+    console.log("APROVADO");
+}else if(projetoFinal >= 85 && frequencia >= 75){
+    console.log("APROVADO");
+}else{
+    console.log("REPROVADO");
+}
+  
+    
+  
+  

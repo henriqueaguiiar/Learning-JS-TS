93d44bb06ececd12306ef882e11fc9dd2405a5e1
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-13 22:12:04 -0300

    Add exercicio condicional 04

---
+++ Typescript/exercicioCondicional04.ts
@@ -0,0 +1,16 @@
+
+
+const notaAlunoA: number = 10;
+const notaAlunoB: number = 8;
+
+const valida: number = 1.5;
+
+
+  let alunoA: number = notaAlunoA;
+  let alunoB: number = notaAlunoB;
+
+  if(alunoA - alunoB < valida){
+      console.log(`DUPLA VALIDA`);
+  }else{
+    console.log(`DUPLA INVALIDA`);
+  }
